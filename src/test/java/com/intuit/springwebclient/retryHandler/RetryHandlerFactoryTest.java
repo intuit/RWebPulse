0549a41e83c840d14d20c164d3f3ad9c737c047e
Reeti Sarup <reeti.sarup@gmail.com>
2023-10-31 00:01:35 +0530

    feat: update RetryHandlerFactory tests

--- src/test/java/com/intuit/springwebclient/retryHandler/RetryHandlerFactoryTest.java
+++ src/test/java/com/intuit/springwebclient/retryHandler/RetryHandlerFactoryTest.java
@@ -1,33 +1,31 @@
 package com.intuit.springwebclient.retryHandler;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 
 public class RetryHandlerFactoryTest {
   
-  private RetryHandlerFactory retryHandlerFactory;
+    @Test
+    public void testAddAndGetHandler() {
+        String handlerTestName = "retryHandlerTestName1";
+        RetryHandler retryHandler1 = new RetryHandler() {
+          
+            @Override
+            public void checkAndThrowRetriableException(Exception ex) {
+                throw new RuntimeException("test exception");
+            }
 
-  @BeforeEach
-  public void setUp() {
-    retryHandlerFactory = new RetryHandlerFactory();
-  }
+            @Override
+            public String getName() {
+                return "defaultHandlerName";
+            }
 
-  @Test
-  public void testAddAndGetHandler() {
-    RetryHandler retryHandler = new RetryHandler();
-    String handlerTestName = "default";
+        };
 
-    retryHandlerFactory.addHandler(handlerTestName, retryHandler);
-
-    Assertions.assertEquals(retryHandler, retryHandlerFactory.getHandler(handlerTestName));
-  }
-
-  @Test
-  public void testGetNonExistentHandler() {
-    String handlerTestName = "non-existent";
-
-    Assertions.assertNull(retryHandlerFactory.getHandler(handlerTestName));
-  }
+        RetryHandlerFactory.addHandler(handlerTestName, retryHandler1);
+      
+        Assertions.assertEquals(retryHandler1, RetryHandlerFactory.getHandler(handlerTestName));
+        Assertions.assertEquals("defaultHandlerName", RetryHandlerFactory.getHandler(handlerTestName).getName());
+        Assertions.assertNull(RetryHandlerFactory.getHandler("retryHandlerTestName2"));
+    }
 }
