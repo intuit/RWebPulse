15e290f4f71878b1d132d415388a4a677353b70c
Reeti Sarup <reeti.sarup@gmail.com>
2023-10-30 21:33:18 +0530

    feat: increase code coverage - add retryHandlerFactory tests

---
+++ src/test/java/com/intuit/springwebclient/retryHandler/RetryHandlerFactoryTest.java
@@ -0,0 +1,33 @@
+package com.intuit.springwebclient.retryHandler;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class RetryHandlerFactoryTest {
+  
+  private RetryHandlerFactory retryHandlerFactory;
+
+  @BeforeEach
+  public void setUp() {
+    retryHandlerFactory = new RetryHandlerFactory();
+  }
+
+  @Test
+  public void testAddAndGetHandler() {
+    RetryHandler retryHandler = new DefaultRetryHandler();
+    String handlerTestName = "default";
+
+    retryHandlerFactory.addHandler(handlerName, retryHandler);
+
+    Assertions.assertEquals(retryHandler, retryHandlerFactory.getHandler(handlerTestName));
+  }
+
+  @Test
+  public void testGetNonExistentHandler() {
+    String handlerTestName = "non-existent";
+
+    Assertions.assertNull(retryHandlerFactory.getHandler(handlerTestName));
+  }
+}
