36ac0d15869afbe38870211efc7faaa12c60493a
mmishra8 <manish_mishra@intuit.com>
2024-02-14 19:39:29 +0530

    Adding Customization over response size.

--- src/main/java/com/intuit/springwebclient/config/SpringWebClientConfig.java
+++ src/main/java/com/intuit/springwebclient/config/SpringWebClientConfig.java
@@ -10,4 +10,5 @@ import org.springframework.context.annotation.Configuration;
 public class SpringWebClientConfig {
     private HttpConnectionPoolConfig connectionPool;
     private HttpClientConfig httpClientConfig;
+    private int maxInMemorySize;
 }
--- src/test/java/com/intuit/springwebclient/config/WebClientConfigurationTest.java
+++ src/test/java/com/intuit/springwebclient/config/WebClientConfigurationTest.java
@@ -21,6 +21,7 @@ public class WebClientConfigurationTest {
     @Mock
     private WebClientRequestFilter webClientRequestFilter;
 
+
     @InjectMocks
     private WebClientConfiguration webClientConfiguration;
 
@@ -44,6 +45,38 @@ public class WebClientConfigurationTest {
 
         Mockito.when(springWebClientConfig.getHttpClientConfig()).thenReturn(httpClientConfig);
 
+        Mockito.when(springWebClientConfig.getMaxInMemorySize()).thenReturn(100);
+
+        Mockito.when(webClientRequestFilter.getFilter()).thenReturn(new ExchangeFilterFunction() {
+            @Override
+            public Mono<ClientResponse> filter(ClientRequest request, ExchangeFunction next) {
+                return null;
+            }
+        });
+
+        WebClient webClient = webClientConfiguration.createWebClient();
+
+    }
+
+    @Test
+    public void testWebClientConnectionProvider_withoutMaxSize() {
+        HttpConnectionPoolConfig poolConfig = new HttpConnectionPoolConfig();
+        poolConfig.setMaxConnections(200);
+        poolConfig.setMaxIdleTime(2000L);
+        poolConfig.setMaxLifeTime(3500L);
+        poolConfig.setPendingAcquireTimeout(1500L);
+
+        Mockito.when(springWebClientConfig.getConnectionPool()).thenReturn(poolConfig);
+
+        ConnectionProvider connectionProvider = webClientConfiguration.webClientConnectionProvider();
+        Assertions.assertEquals(200, connectionProvider.maxConnections());
+
+        HttpClientConfig httpClientConfig = new HttpClientConfig();
+        httpClientConfig.setConnectTimeoutMillis(5000);
+        httpClientConfig.setConnectTimeoutMillis(3000);
+
+
+        Mockito.when(springWebClientConfig.getHttpClientConfig()).thenReturn(httpClientConfig);
         Mockito.when(webClientRequestFilter.getFilter()).thenReturn(new ExchangeFilterFunction() {
             @Override
             public Mono<ClientResponse> filter(ClientRequest request, ExchangeFunction next) {
